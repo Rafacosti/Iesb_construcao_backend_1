b09d5149ce8137ef447fca3e1af2b8f6c9c04358
Rafacosti <rafaelcosta005@gmail.com>
2025-08-25 20:03:01 -0300

    enviando

---
+++ calculadora.js
@@ -0,0 +1,39 @@
+function somar(a, b) {
+    return a + b;
+  }
+  
+  function subtrair(a, b) {
+    return a - b;
+  }
+  
+  function multiplicar(a, b) {
+    return a * b;
+  }
+  
+  function dividir(a, b) {
+    if (b === 0) {
+      return 'Erro: divisão por zero!';
+    }
+    return a / b;
+  }
+  
+  function aoQuadrado(a) {
+    return a * a;
+  }
+  
+  function raizQuadrada(a) {
+    if (a < 0) {
+      return 'Erro: raiz de número negativo!';
+    }
+    return Math.sqrt(a);
+  }
+  
+  export default {
+    somar,
+    subtrair,
+    multiplicar,
+    dividir,
+    aoQuadrado,
+    raizQuadrada
+  };
+  
